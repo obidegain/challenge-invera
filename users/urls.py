9248eef2236c24e437d8e7b2aa73496116fe353e
obidegain <octa.bidegain@gmail.com>
2024-09-29 23:44:58 -0300

    (feat) get tasks list endpoint

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, GetTasksView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -9,4 +9,5 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('get_tasks/', GetTasksView.as_view(), name='get_list_tasks')
 ]
--- users/views.py
+++ users/views.py
@@ -1,11 +1,20 @@
 from django.contrib.auth.models import User
-from rest_framework import generics, status
+from rest_framework import generics, permissions
 from .serializers import RegisterSerializer
+from tasks.serializers import TaskSerializer
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from tasks.models import Tasks
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
+
+
+class GetTasksView(generics.ListAPIView):
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Tasks.objects.filter(owner=self.request.user)
